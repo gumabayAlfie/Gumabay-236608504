4746d3715e315715a1f06538c47ac8765a7a8437
gumabayAlfie <gumabay.alfie@gmail.com>
2025-10-06 23:39:14 +0800

    uwuwuwuwu

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Exercise</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #E0B0FF; 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .card {
+            background:rgb(170, 255, 254);
+            padding: 30px;
+            border-radius: 12px;
+            box-shadow: 0 4px 10px rgba(0,0,0,0.2);
+            text-align: center;
+            width: 400px;
+        }
+        h1 {
+            color:rgb(13, 173, 160); 
+            margin-bottom: 20px;
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+        li {
+            margin: 10px 0;
+        }
+        a {
+            display: block;
+            padding: 10px;
+            background: #9370DB; 
+            color: #f5f5dc;
+            text-decoration: none;
+            border-radius: 6px;
+            transition: 0.3s;
+        }
+        a:hover {
+            background: #6A0DAD; 
+        }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Alfie Viannie Gumabay</h1>
+        <ul>
+            <li><a href="intro.php">1. Introduce Yourself</a></li>
+            <li><a href="simple_math.php">2. Simple Math</a></li>
+            <li><a href="rectangle.php">3. Area and Perimeter of a Rectangle</a></li>
+            <li><a href="temp_converter.php">4. Temperature Converter</a></li>
+            <li><a href="swap_var.php">5. Swapping Variable</a></li>
+            <li><a href="salary_calc.php">6. Salary Calculator</a></li>
+            <li><a href="bmi_calc.php">7. BMI Calculator</a></li>
+            <li><a href="manipulation.php">8. String Manipulation</a></li>
+            <li><a href="bank_acc.php">9. Bank Account Simulation</a></li>
+            <li><a href="grade_sys.php">10. Simple Grading System</a></li>
+            <li><a href="currency_converter.php">11. Currency Converter</a></li>
+            <li><a href="travel_cost.php">12. Travel Cost Estimator</a></li>
+        </ul>
+    </div>
+</body>
+</html>
