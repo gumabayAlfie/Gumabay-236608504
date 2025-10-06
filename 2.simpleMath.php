a151c84acd47ad75a109a0582ae0bc922c9b5f31
gumabayAlfie <gumabay.alfie@gmail.com>
2025-10-07 01:47:25 +0800

    simple math updated

---
+++ 2.simpleMath.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="card">
+        <h1>Simple Math</h1>
+
+<?php
+$a = 10;
+$b = 5;
+
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "Sum: $sum,  Difference: $difference, Product: $product, Quotient: $quotient";
+?>
+</body>
+</html>
