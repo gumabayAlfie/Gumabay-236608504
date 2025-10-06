0f657c502ac109ac6452b7c51bbe12cf3b59ed5a
gumabayAlfie <gumabay.alfie@gmail.com>
2025-10-07 01:27:16 +0800

    updated index and intro

---
+++ 1.introduction.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="card">
+        <h1>Introduce Yourself</h1>
+<?php 
+$name="Alfie Viannie G. Gumabay";
+$age="24";
+$favcolor="Cyan";
+
+echo "Hi, I'm $name, I am $age years old, and my favorite color is $favcolor";
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -51,18 +51,18 @@
     <div class="card">
         <h1>Alfie Viannie Gumabay</h1>
         <ul>
-            <li><a href="introduction.php">1. Introduce Yourself</a></li>
-            <li><a href="simple_math.php">2. Simple Math</a></li>
-            <li><a href="rectangle.php">3. Area and Perimeter of a Rectangle</a></li>
-            <li><a href="temperature.php">4. Temperature Converter</a></li>
-            <li><a href="swap_var.php">5. Swapping Variable</a></li>
-            <li><a href="salary_calc.php">6. Salary Calculator</a></li>
-            <li><a href="bmi_calc.php">7. BMI Calculator</a></li>
-            <li><a href="manipulation.php">8. String Manipulation</a></li>
-            <li><a href="bank_acc.php">9. Bank Account Simulation</a></li>
-            <li><a href="grade_sys.php">10. Simple Grading System</a></li>
-            <li><a href="currency_converter.php">11. Currency Converter</a></li>
-            <li><a href="travel_cost.php">12. Travel Cost Estimator</a></li>
+            <li><a href="1.introduction.php">1. Introduce Yourself</a></li>
+            <li><a href="2.simpleMath.php">2. Simple Math</a></li>
+            <li><a href="3.rectangle.php">3. Area and Perimeter of a Rectangle</a></li>
+            <li><a href="4.temperature.php">4. Temperature Converter</a></li>
+            <li><a href="5.swap.php">5. Swapping Variable</a></li>
+            <li><a href="6.salary.php">6. Salary Calculator</a></li>
+            <li><a href="7.bmi.php">7. BMI Calculator</a></li>
+            <li><a href="8.manipulation.php">8. String Manipulation</a></li>
+            <li><a href="9.bank.php">9. Bank Account Simulation</a></li>
+            <li><a href="10.grade.php">10. Simple Grading System</a></li>
+            <li><a href="11.currencyConverter.php">11. Currency Converter</a></li>
+            <li><a href="12.travelCost.php">12. Travel Cost Estimator</a></li>
         </ul>
     </div>
 </body>
