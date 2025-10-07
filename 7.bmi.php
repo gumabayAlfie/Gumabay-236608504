45d362603e7e5e3eba0d15decf43ab9d35b7fc7b
gumabayAlfie <gumabay.alfie@gmail.com>
2025-10-07 19:50:41 +0800

    try

---
+++ 7.bmi.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="card">
+        <h1>Salary Calculator</h1>
+
+        <?php
+heyyy testttttt
+?>
+
+</body>
+</html>
